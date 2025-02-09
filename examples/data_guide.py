7d8960afc6bc1f67d5db5347295b16973887678b
Marcus Hughes <hughes.jmb@gmail.com>
2025-02-08 17:11:02 -0700

    clean up notebook format

--- examples/data_guide.py
+++ examples/data_guide.py
@@ -6,7 +6,7 @@ Guide to Data
 A notebook guide to working with PUNCH FITS files in Python
 """
 
-# %% [markdown]
+# %%
 # This Jupyter Notebook presents a comprehensive guide to the analysis and visualization of PUNCH FITS files using Python. The guide leverages a suite of Python libraries, including `matplotlib` for data visualization, numpy for numerical operations, `astropy` for FITS file handling and WCS manipulation, `sunpy` for solar data analysis, and `ndcube` for multi-dimensional data handling. The notebook serves as a robust resource for researchers and scientists, providing them with the tools and knowledge to effectively manipulate and interpret PUNCH FITS files. The guide's step-by-step approach, coupled with code snippets and detailed explanations, ensures a user-friendly experience, promoting the accessibility of PUNCH data for a wider scientific community.
 #
 # To install these dependencies, use `pip install -r requirements.txt` in your terminal.
@@ -27,15 +27,17 @@ from sunpy.map import Map
 
 from punchbowl.data.sample import PUNCH_PAM
 
-# %% [markdown]
-# ## Data loading
+# %%
+# Data loading
+# ------------
 #
 # We first specify the file we wish to load. For this notebook, let's use the included L3 PAM file. Then, we'll load it two different ways:
 #
 # 1. With `astropy.io`
 # 2. With `SunPy Maps`
 #
-# The data are RICE compressed, so we'll have to look in the *second* at index 1 (since we're zero-indexed) HDU for the main data frame. There is also an uncertainty layer in the last HDU that we'll explore later.
+# The data are RICE compressed, so we'll have to look in the *second* at index 1 (since we're zero-indexed) HDU for the main data frame.
+# There is also an uncertainty layer in the last HDU that we'll explore later.
 
 # %%
 with fits.open(PUNCH_PAM) as hdul:
@@ -43,13 +45,14 @@ with fits.open(PUNCH_PAM) as hdul:
     header = hdul[1].header
     uncertainty = hdul[2].data
 
-# %% [markdown]
-# First, let's look at the shape of the data. For this data product, total brightness and polarized brightness are stacked along the first dimension. The uncertainty array corresponds on a pixel-to-pixel basis with the data array.
+# %%
+# First, let's look at the shape of the data. For this data product, total brightness and polarized brightness are stacked along the first dimension.
+# The uncertainty array corresponds on a pixel-to-pixel basis with the data array.
 
 # %%
-data.shape, uncertainty.shape
+print(data.shape, uncertainty.shape)
 
-# %% [markdown]
+# %%
 # We can look in the header for details about the data. A few things to note:
 #
 # - The header is divided into helpful sections such as "temporal information" and "instrument and spacecraft state." This aids in navigating the information. These sections are consistent for other products (with the addition of more sections depending on the product).
@@ -57,44 +60,48 @@ data.shape, uncertainty.shape
 # - The first dimension iterates over total brightness and polarized brightness, in that order. This is indicated by the `OBSLAYR1` and `OBSLAYR2` keywords.
 
 # %%
-header
+print(header)
 
-# %% [markdown]
+# %%
 # We can create `Astropy WCS` objects from the header. Since the data are three dimensional, the WCS will be three dimensions. Luckily, we can slice WCSes.
 
 # %%
 data_wcs = WCS(header)
-data_wcs
+print(data_wcs)
 
-# %% [markdown]
+# %%
 # We can also access the celestial WCS by setting `key='A'`. "
 
 # %%
 data_wcs_celestial = WCS(header, key="A")
-data_wcs_celestial
+print(data_wcs_celestial)
 
-# %% [markdown]
+# %%
 # We can also use SunPy Maps to load the data. However, since Maps only support 2D images, it will warn us that it's truncating a dimension of the data and only showing the total brightness. We get a list of Maps where the first entry is the data and the second is the uncertainty.
 
 # %%
 data_map = Map(PUNCH_PAM)
-data_map[0]
+data_map[0].plot()
+plt.show()
 
-# %% [markdown]
+# %%
 # If you wish to make a map of the polarized brightness, we can also do that manually.
 
 # %%
 data_map = Map(data[1], header)
-data_map
+data_map.plot()
+plt.show()
 
-# %% [markdown]
-# ## Plotting data
+# %%
+# Plotting data
+# -------------
 
 # %% [markdown]
 # There are many ways to plot the data. We are working on more visualization tools that will be available with the release of the `punchbowl` package. First, we can plot the SunPy map.
 
 # %%
 data_map.plot(norm="log")
+plt.show()
 
 # %% [markdown]
 # We can also use Matplotlib directly to modify the plot however we want. Notice that we set the projection so it is coordinate aware. Remember we have to slice the WCS as `data_wcs[0]` instead of `data_wcs` because our data are 3D but we want to view only the spatial dimensions.
@@ -117,6 +124,7 @@ ax.set_xlabel("Helioprojective longitude")
 ax.set_ylabel("Helioprojective latitude")
 ax.set_title("Synthetic PUNCH Polarized Brightness")
 fig.colorbar(im, ax=ax, label="Mean Solar Brightness")
+plt.show()
 
 # %% [markdown]
 # Since we set the projection, we could also use the celestial WCS instead.
@@ -127,7 +135,6 @@ fig.colorbar(im, ax=ax, label="Mean Solar Brightness")
 fig, ax = plt.subplots(figsize=(9.5, 7.5), subplot_kw={"projection":data_wcs_celestial[0]})
 
 im = ax.imshow(data[0], cmap="Greys_r", norm=LogNorm(vmin=1.77e-15, vmax=3.7e-11))
-
 
 # set up the axis labels
 lon, lat = ax.coords
@@ -141,9 +148,11 @@ ax.set_xlabel("RA")
 ax.set_ylabel("DEC")
 ax.set_title("Synthetic PUNCH Polarized Brightness")
 fig.colorbar(im, ax=ax, label="Mean Solar Brightness")
+plt.show()
 
 # %% [markdown]
-# ## Reprojecting the data
+# Reprojecting the data
+# ---------------------
 
 # %% [markdown]
 # What if we want want to manipulate the images? We can use built-in tools of SunPy Maps. For example, we can reproject.
@@ -162,16 +171,18 @@ new_wcs.wcs.pc = (0.66,-0.33), (0.33,0.66)
 
 # We can then reproject using SunPy maps
 new_map = data_map.reproject_to(new_wcs)
-new_map
+new_map.plot()
+plt.show()
 
 # %% [markdown]
-# ## Using NDCube
+# Using NDCube
+# ------------
 #
 # So far, we've only talked about directly manipulating the data or using SunPy Maps. However, we encourage you to utilize NDCubes. We use them in the data reduction pipeline too. [NDCube](https://docs.sunpy.org/projects/ndcube/en/stable/introduction.html) is a SunPy package for generalized n-dimensional data. We can use our manually loaded data to make an NDCube with all of the data in one place. There's even a way to bundle uncertainty directly into the cube.
 
 # %%
 data_ndcube = NDCube(data, wcs=data_wcs, meta=header, uncertainty=uncertainty)
-data_ndcube
+print(data_ndcube)
 
 # %% [markdown]
 # We can then use all the great NDCube functionality! Much like SunPy Maps we can reproject even. Note that as of writing this guide, the uncertainty is dropped when NDCube reprojects data.
@@ -187,19 +198,22 @@ new_wcs.wcs.crval = 0, 0, 0
 new_wcs.wcs.cdelt = 0.0225, 0.0225, 1.0
 
 new_cube = data_ndcube.reproject_to(new_wcs)
-new_cube
+new_cube.plot()
+plt.show()
 
 # %% [markdown]
 # NDCubes have a plotter on them too. Notice we even get an interactive slider to manipulate the polarization axis.
 
 # %%
 new_cube.plot(interpolation="None", norm=LogNorm(vmin=1.77E-15, vmax=3.7E-11), cmap="Greys_r")
+plt.show()
 
 # %% [markdown]
 # We encourage you to explore NDCube more. There are other helpful things like [cropping](https://docs.sunpy.org/projects/ndcube/en/stable/generated/gallery/slicing_ndcube.html#cropping-cube-using-world-coordinate-values-using-ndcube-ndcube-crop) that you can do.
 
 # %% [markdown]
-# ## Uncertainty
+# Uncertainty
+# -----------
 
 # %% [markdown]
 # Level 1, 2, and 3 PUNCH products come with an uncertainty estimate. This uncertainty is expressed as the fractional uncertainty ranging from 0 to 1. To get the absolute uncertainty, simply multiply the fractional uncertainty by the data value.
@@ -239,3 +253,4 @@ for ax in axs:
     ax.coords.grid(color="white", alpha=.25, ls="dotted")
     ax.set_xlabel("Helioprojective longitude")
     ax.set_ylabel("Helioprojective latitude")
+plt.show()

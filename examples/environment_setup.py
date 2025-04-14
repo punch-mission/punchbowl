ab2ea8a9fe15559b19862e37bb2d56a0782b04da
Chris Lowder <chris.lowder@swri.org>
2025-04-14 11:29:06 -0600

    adds environment setup notebook

--- examples/environment_setup.py
+++ examples/environment_setup.py
@@ -7,16 +7,28 @@ To run the sample notebooks contained within punchbowl, you must first create a 
 """
 
 # %%
-# To run the sample notebooks contained within punchbowl, you must first create a Python environment.
+# To run the sample notebooks contained within punchbowl, you must first create a Python environment. These days, it's recommended to have a local virtual environment for a project rather than a global configuration.
 
 # If using an environment for running Jupyter notebooks (Visual Studio Code, PyCharm, etc), this environment setup may be handled automatically or through UI prompts on notebook execution.
 
 # To set up manually, start within a terminal, and navigate to the directory in which you want to work with notebooks.
 
+# Begin by setting up a virtual environment within the specified directory. Here we'll setup a hidden directory .venv
+# ```bash
 # python -m venv .venv
+# ```
 
+# Next activate the virtual environment by executing the activation script. Note that when returning to this environment in future, you'll also need to activate the environment once again.
+# ```bash
 # source .venv/bin/activate
+# ```
 
-# pip install requirements.txt
+# To get started quickly with punchbowl, one can install a few packages using pip. Other packages of note can be installed in this same way.
+# ```bash
+# pip install ipykernel jupyter punchbowl
+# ```
 
-#
+# From here, one can open a downloaded notebook in an editor of choice (selecting the local virtual environment), or by calling jupyter directly.
+# ```bash
+# jupyter downloaded_notebook.ipynb
+# ```

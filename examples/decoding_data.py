00f1ebabdf095d18635a540a8061cfee12e82cb5
Chris Lowder <chris.lowder@swri.org>
2025-04-14 11:28:59 -0600

    adds square root decoding notebook

--- examples/decoding_data.py
+++ examples/decoding_data.py
@@ -13,22 +13,49 @@ Data downlinked from the individual PUNCH spacecraft are usually square root enc
 # Load libraries
 
 import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LogNorm
 
 from punchbowl.data import load_ndcube_from_fits
-from punchbowl.data.sample import PUNCH_PAM  # TODO - add L0 to sample data?
+from punchbowl.data.sample import PUNCH_DK4
+from punchbowl.data.visualize import cmap_punch
 from punchbowl.level1.sqrt import decode_sqrt
 
 # %%
-path = "/Users/clowder/Downloads/PUNCH_L0_DK3_20250407194626_v1.fits"
-cube = load_ndcube_from_fits(path)
+# We can begin by loading sample data into an NDCube object. Here we'll load a sample level 0 LED dark image from NFI.
 
 # %%
-decoded = decode_sqrt(cube.data,
-                      from_bits = 22,
+cube = load_ndcube_from_fits(PUNCH_DK4)
+
+# %%
+# Note that for level 0 data the data is still square-root encoded, and will be unpacked in subsequent levels. You can manually decoded this data with the same pipeline function.
+
+# %%
+data_decoded = decode_sqrt(cube.data,
+                      from_bits = 16,
                       to_bits = 11,
-                      scaling_factor = 64,
                       ccd_gain_right = 4.94,
                       ccd_gain_left = 4.89,
                       ccd_offset = 400,
-                      ccd_read_noise = 17,
+                      ccd_read_noise = 17
                     )
+
+# %%
+# Now that we have this data square-root decoded, we can plot the image. Note that because this is a dark image, there won't be much to see.
+
+# %%
+fig, ax = plt.subplots(figsize=(9.5, 7.5), subplot_kw={"projection":cube.wcs})
+
+im = ax.imshow(data_decoded, cmap=cmap_punch, norm=LogNorm())
+
+lon, lat = ax.coords
+lat.set_major_formatter("dd")
+lon.set_major_formatter("dd")
+ax.set_facecolor("black")
+ax.coords.grid(color="white", alpha=.25, ls="dotted")
+ax.set_xlabel("Helioprojective longitude")
+ax.set_ylabel("Helioprojective latitude")
+ax.set_title("PUNCH Level 0 LED Image")
+fig.colorbar(im, ax=ax, label="DN")
+plt.show()

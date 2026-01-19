a9f3bf0f49ff307ca00bd5f672e815e7f9c6b5d3
jmbhughes <13068381+jmbhughes@users.noreply.github.com>
2026-01-19 05:51:22 +0000

    Apply automatic changes

---
+++ docs/sg_execution_times.rst
@@ -0,0 +1,58 @@
+
+:orphan:
+
+.. _sphx_glr_sg_execution_times:
+
+
+Computation times
+=================
+**07:03.540** total execution time for 8 files **from all galleries**:
+
+.. container::
+
+  .. raw:: html
+
+    <style scoped>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+    </style>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <script type="text/javascript" class="init">
+    $(document).ready( function () {
+        $('table.sg-datatable').DataTable({order: [[1, 'desc']]});
+    } );
+    </script>
+
+  .. list-table::
+   :header-rows: 1
+   :class: table table-striped sg-datatable
+
+   * - Example
+     - Time
+     - Mem (MB)
+   * - :ref:`sphx_glr_auto_examples_make_punch_movies.py` (``../examples/make_punch_movies.py``)
+     - 05:34.636
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_querying_data.py` (``../examples/querying_data.py``)
+     - 00:31.740
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_decoding_data.py` (``../examples/decoding_data.py``)
+     - 00:18.807
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_create_rgb_map.py` (``../examples/create_rgb_map.py``)
+     - 00:17.293
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_data_guide.py` (``../examples/data_guide.py``)
+     - 00:09.426
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_PUNCH-InSitu-Connection.py` (``../examples/PUNCH-InSitu-Connection.py``)
+     - 00:08.172
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_data_guide_quickpunch.py` (``../examples/data_guide_quickpunch.py``)
+     - 00:03.466
+     - 0.0
+   * - :ref:`sphx_glr_auto_examples_environment_setup.py` (``../examples/environment_setup.py``)
+     - 00:00.000
+     - 0.0
---
+++ notebooks/auto_examples/punchbowl/level3/low_noise.py
@@ -0,0 +1,62 @@
+from datetime import UTC, datetime
+
+import numpy as np
+from astropy.nddata import StdDevUncertainty
+from dateutil.parser import parse as parse_datetime
+from ndcube import NDCube
+
+from punchbowl.data.meta import NormalizedMetadata
+from punchbowl.data.punch_io import check_outlier
+from punchbowl.prefect import punch_task
+
+
+@punch_task
+def create_low_noise_task(cubes: list[NDCube]) -> NDCube:
+    """Create a low noise image from a set of inputs."""
+    cubes = [cube for cube in cubes if not check_outlier(cube)]
+
+    ref_cube_index = len(cubes)//2 - 1
+
+    data_stack = np.stack([cube.data for cube in cubes], axis=0)
+    uncertainty_stack = np.array([cube.uncertainty.array for cube in cubes])
+
+    uncertainty_stack[uncertainty_stack <= 0] = np.inf
+    uncertainty_stack[~np.isfinite(uncertainty_stack)] = 1E32
+
+    uncertainty_stack[np.isnan(data_stack)] = np.inf
+    uncertainty_stack[data_stack == 0] = np.inf
+
+    weight_stack = 1/np.square(uncertainty_stack)
+    weight_stack[np.isnan(uncertainty_stack)] = np.nan
+
+    new_data = np.nansum(data_stack * weight_stack, axis=0) / np.nansum(weight_stack, axis=0)
+
+    final_uncertainty = np.sqrt(np.nansum(uncertainty_stack, axis=0)) / np.sqrt(new_data)
+
+    new_code = cubes[0].meta.product_code[0] + "A" + cubes[0].meta.product_code[2]
+    new_meta = NormalizedMetadata.load_template(new_code, "3")
+
+    for k in cubes[0].meta.fits_keys:
+        if k not in ("COMMENT", "HISTORY", "") and k in new_meta:
+            new_meta[k] = cubes[ref_cube_index].meta[k].value
+
+    times_obs = np.array([cube.meta.datetime.timestamp() for cube in cubes])
+    times_beg = np.array([parse_datetime(cube.meta["DATE-BEG"].value).replace(tzinfo=UTC).timestamp()
+                          for cube in cubes])
+    times_end = np.array([parse_datetime(cube.meta["DATE-END"].value).replace(tzinfo=UTC).timestamp()
+                          for cube in cubes])
+
+    new_meta["TYPECODE"] = new_code[0:2]
+    # TODO - distinction here? and mean versus midpoint.
+    new_meta["DATE-OBS"] = datetime.fromtimestamp(np.mean(times_obs),
+                                                  tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+    new_meta["DATE-AVG"] = datetime.fromtimestamp(np.mean(times_obs),
+                                                  tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+
+    new_meta["DATE-BEG"] = datetime.fromtimestamp(np.min(times_beg),
+                                                  tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+    new_meta["DATE-END"] = datetime.fromtimestamp(np.max(times_end),
+                                                  tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+
+    return NDCube(data=new_data, uncertainty=StdDevUncertainty(final_uncertainty),
+                  wcs=cubes[ref_cube_index].wcs, meta=new_meta)

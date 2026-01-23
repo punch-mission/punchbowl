f3148df5978ecb5cb195bb53c39396d7103be8d5
Marcus Hughes <hughes.jmb@gmail.com>
2026-01-23 04:30:18 -0700

    add auto scripts and rename pipe to auto

--- punchbowl/pipe/control/cache_layer/__init__.py
+++
@@ -1,3 +0,0 @@
-from punchbowl.pipe.control.cache_layer import nfi_l1, psf, quartic_coefficients, stray_light, vignetting_function
-
-__all__ = ["nfi_l1", "psf", "quartic_coefficients", "stray_light", "vignetting_function"]

4cc2cce11e98de187f411dcc0c97ff533675f545
Sam Van Kooten <vankooten.sam@gmail.com>
2026-01-26 14:59:32 -0700

    Don't despike images with bad packets
    # Conflicts:
    #	punchbowl/level1/despike.py

--- punchbowl/level1/despike.py
+++ punchbowl/level1/despike.py
@@ -3,7 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 from ndcube import NDCube
 from prefect import get_run_logger
-from scipy.ndimage import gaussian_filter
+from scipy.ndimage import binary_dilation, gaussian_filter
 from threadpoolctl import threadpool_limits
 
 from punchbowl.data import load_ndcube_from_fits
@@ -45,8 +45,15 @@ def despike_polseq(
          and a list of spike locations for all neighbors
 
     """
-    sequence = np.stack([cube.data for cube in [*neighbors, reference]], axis=0)
+    sequence = np.stack([cube.data.copy() for cube in [*neighbors, reference]], axis=0)
+
     seq_len = sequence.shape[0]
+
+    # saturated regions can lead to weird uncertainties and leftovers so we try to mask them
+    inf_uncertainty_mask = sequence >= 60_000
+    inf_uncertainty_mask = binary_dilation(inf_uncertainty_mask)
+    inf_uncertainty_mask = np.any(inf_uncertainty_mask, axis=0)
+    sequence[np.stack([inf_uncertainty_mask for _ in range(seq_len)])] = 0
 
     # create the high-pass-filtered images
     def blur_one_image(image: np.ndarray)->np.ndarray:
@@ -62,34 +69,45 @@ def despike_polseq(
     #of the N-1 lowest values of each pixel
     match seq_len:
         case 7:
-            hpf_median_s = np.mean(hpf_sorted[2:4],axis=0)
+            hpf_median_s = np.nanmean(hpf_sorted[2:4],axis=0)
         case 6:
             hpf_median_s = hpf_sorted[2]
         case 5:
-            hpf_median_s = np.mean(hpf_sorted[1:3],axis=0)
+            hpf_median_s = np.nanmean(hpf_sorted[1:3],axis=0)
         case 4:
             hpf_median_s = hpf_sorted[1]
         case 3:
-            hpf_median_s = np.mean(hpf_sorted[0:2],axis=0)
+            hpf_median_s = np.nanmean(hpf_sorted[0:2],axis=0)
         case _:
             raise RuntimeError(f"A sequence length of {seq_len} is not supported.")
 
     hpf_stdev = np.std(hpf_sorted[:-1], axis=0, ddof=0)
     hpf_zscore = (hpf - hpf_median_s)/hpf_stdev
 
-    cosmic_sequence = np.zeros_like(sequence, dtype=bool)
-    cosmic_sequence[hpf_zscore>=hpf_zscore_thresh] = 1
+    sequence_spike_mask = np.zeros_like(sequence, dtype=bool)
+    sequence_spike_mask[hpf_zscore>=hpf_zscore_thresh] = 1
 
-    reference.data[(hpf_zscore>=hpf_zscore_thresh)[-1]] = np.nan
+    image_bounds = sequence[-1] == 0  # the image is zero where there's no data
+    correction_mask = (hpf_zscore >= hpf_zscore_thresh)[-1]
+    correction_mask[inf_uncertainty_mask] = 1
+    correction_mask[image_bounds] = 0
+
+    sequence_spike_mask[-1] = correction_mask
+
+    reference.data[correction_mask] = np.nan
 
     reference.data = mean_correct(data_array=reference.data, mask_array=~np.isnan(reference.data))
 
-    return reference, cosmic_sequence
+    # any remaining nans are bad!
+    reference.uncertainty.array[np.isnan(reference.data)] = np.inf
+    reference.data[np.isnan(reference.data)] = 0
+
+    return reference, sequence_spike_mask
 
 
 @punch_task
 def despike_polseq_task(data_object: NDCube,
-                        neighbors: list[NDCube],
+                        neighbors: list[NDCube] | list[str],
                         filter_width: float=25.0,
                         hpf_zscore_thresh: float=10.0,
                         max_workers: int | None = None)-> NDCube:
@@ -100,7 +118,7 @@ def despike_polseq_task(data_object: NDCube,
     ----------
     data_object : NDCube
         Image to be despiked.
-    neighbors : list[NDCube]
+    neighbors : list[NDCube]  | list[str]
         Sequence of neighbor images from the same spacecraft and roll sequence to use in despiking.
     filter_width : float, optional
         width of the gaussian filter used to construct the high-pass-filtered image, in pixels.
@@ -117,9 +135,16 @@ def despike_polseq_task(data_object: NDCube,
     """
     logger = get_run_logger()
 
-    if neighbors is not None:
+    neighbors = neighbors if neighbors is not None else []
+
+    if data_object.meta["BADPKTS"].value:
+        data_object.meta.history.add_now("LEVEL1-despike", "Image has bad packets; no despiking applied")
+        logger.info("Bad packets---despiking skipped")
+    elif 3 <= len(neighbors) <= 7:
         logger.info(f"Neighbors = {neighbors}")
-        neighbors = [decode_sqrt_data(load_ndcube_from_fits(n)) if isinstance(n, str) else n for n in neighbors]
+
+        neighbors = [load_ndcube_from_fits(n) if isinstance(n, str) else n for n in neighbors]
+        neighbors = [decode_sqrt_data(n) for n in neighbors]
 
         with threadpool_limits(max_workers):
             data_object, spikes = despike_polseq(
@@ -134,6 +159,6 @@ def despike_polseq_task(data_object: NDCube,
         data_object.meta.history.add_now("LEVEL1-despike", f"zscore_thresh={hpf_zscore_thresh}")
         data_object.meta.history.add_now("LEVEL1-despike", f"neighbor_count={len(neighbors)}")
     else:
-        data_object.meta.history.add_now("LEVEL1-despike", "Empty neighbors so no correction applied")
-        logger.info("No polarization neighbors so despiking is skipped")
+        data_object.meta.history.add_now("LEVEL1-despike", "Incompatible neighbor count so no correction applied")
+        logger.info(f"Incompatible neighbor count {len(neighbors)} so despiking is skipped")
     return data_object

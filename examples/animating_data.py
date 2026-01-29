75cfa27840a0ebb3fa8f908b092242127ce4e599
Chris Lowder <chris.lowder@swri.org>
2026-01-29 16:59:25 -0700

    adds animation example notebook

---
+++ examples/animating_data.py
@@ -0,0 +1,55 @@
+"""
+===================
+Animating PUNCH data
+===================
+
+How to animate PUNCH data using built-in plotting tools
+"""
+
+# %%  [markdown]
+# First we'll load a set of libraries. This is minimal, but will give us the tools to query a sample of data to download and animate.
+
+from datetime import datetime
+
+from IPython.display import Video
+from sunpy.net import Fido, attrs
+
+# %%
+from punchbowl.data.visualize import animate_punch
+
+# %% [markdown]
+# Next we'll query a sample of data to animate. Note that you can modify the time range, the product code, and the data version code.
+
+# %%
+start_time, end_time = datetime(2025, 10, 31, 2, 0, 0), datetime(2025, 10, 31, 3, 0, 0)
+product_code = "L3_CIM"
+version_code = "v0i"
+
+result = Fido.search(
+                attrs.Time(start_time, end_time),
+                attrs.Source("PUNCH-MOSAIC"),
+                attrs.Instrument("Unpolarized")
+            )
+
+result = result[0][[i for i, r in enumerate(result[0]['fileid']) if product_code in r and version_code in r]]
+
+result
+
+# %% [markdown]
+# The resulting files can then be downloaded.
+
+# %%
+files = Fido.fetch(result)
+
+# %% [markdown]
+# That file list is then passed into the animator function, along with an output filename.
+
+# %%
+output_path = "PUNCH_CIM.mp4"
+animate_punch(files, output_path=output_path)
+
+# %% [markdown]
+# The resulting animation is saved to disk, and can optionally be viewed here in a notebook cell.
+
+# %%
+Video(output_path, embed=True)

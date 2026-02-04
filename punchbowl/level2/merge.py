3bc3036d45a9068a4b266ca7463ef3c3017dee23
Chris Lowder <chris.lowder@swri.org>
2026-02-04 15:38:44 -0700

    reorganizes merge_ndcubes logic

--- punchbowl/level2/merge.py
+++ punchbowl/level2/merge.py
@@ -12,30 +12,31 @@ from punchbowl.util import average_datetime
 
 def _merge_ndcubes(cubes: list[NDCube | None], reference_cube_index: int | None = None) -> NDCube:
     """Create a merged data product from a set of input data, weighting by uncertainty."""
-    if cubes is not None:
-        if reference_cube_index is None:
-            reference_cube_index = len(cubes)//2 - 1
+    if cubes is None:
+        return None
 
-        data_stack = np.stack([cube.data for cube in cubes], axis=0)
-        uncertainty_stack = np.array([cube.uncertainty.array for cube in cubes])
+    if reference_cube_index is None:
+        reference_cube_index = len(cubes)//2 - 1
 
-        uncertainty_stack[uncertainty_stack <= 0] = np.nan
-        uncertainty_stack[~np.isfinite(uncertainty_stack)] = np.nan
-        uncertainty_stack[data_stack == 0] = np.nan
+    data_stack = np.stack([cube.data for cube in cubes], axis=0)
+    uncertainty_stack = np.array([cube.uncertainty.array for cube in cubes])
 
-        weight_stack = 1/np.square(uncertainty_stack)
+    uncertainty_stack[uncertainty_stack <= 0] = np.nan
+    uncertainty_stack[~np.isfinite(uncertainty_stack)] = np.nan
+    uncertainty_stack[data_stack == 0] = np.nan
 
-        new_data = np.nansum(data_stack * weight_stack, axis=0) / np.nansum(weight_stack, axis=0)
+    weight_stack = 1/np.square(uncertainty_stack)
 
-        final_uncertainty = np.sqrt(np.sum(np.where(np.isfinite(uncertainty_stack), uncertainty_stack**2, 0), axis=0)) \
-            / np.sqrt(np.sum(np.isfinite(uncertainty_stack), axis=0))
+    new_data = np.nansum(data_stack * weight_stack, axis=0) / np.nansum(weight_stack, axis=0)
 
-        final_uncertainty[final_uncertainty == 0] = np.inf
-        final_uncertainty[~np.isfinite(final_uncertainty)] = np.inf
+    final_uncertainty = np.sqrt(np.sum(np.where(np.isfinite(uncertainty_stack), uncertainty_stack**2, 0), axis=0)) \
+        / np.sqrt(np.sum(np.isfinite(uncertainty_stack), axis=0))
 
-        return NDCube(data=new_data, uncertainty=StdDevUncertainty(final_uncertainty), \
-                      wcs=cubes[reference_cube_index].wcs)
-    return None
+    final_uncertainty[final_uncertainty == 0] = np.inf
+    final_uncertainty[~np.isfinite(final_uncertainty)] = np.inf
+
+    return NDCube(data=new_data, uncertainty=StdDevUncertainty(final_uncertainty), \
+                    wcs=cubes[reference_cube_index].wcs)
 
 
 @punch_task

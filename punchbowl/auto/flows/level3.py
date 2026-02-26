c6a945ce32e8aba7778323a7b252097f9df8daec
Marcus Hughes <hughes.jmb@gmail.com>
2026-02-26 14:05:49 -0700

    remove mistaken tab

--- punchbowl/auto/flows/level3.py
+++ punchbowl/auto/flows/level3.py
@@ -131,7 +131,7 @@ def level3_PIM_query_ready_files(session, pipeline_config: dict, reference_time=
     logger = get_run_logger()
     all_ready_files = session.query(File).where(and_(and_(File.state == "created",
                                                           File.level == "2"),
-                                                     File.file_type == "PT"))   .order_by(File.date_obs.asc()).all()
+                                                     File.file_type == "PT")).order_by(File.date_obs.asc()).all()
     logger.info(f"{len(all_ready_files)} Level 2 PTM files need to be processed.")
 
     actually_ready_files = []

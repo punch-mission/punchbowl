1db94b3d320792054e6b150a560a978ed130d920
Chris Lowder <chris.lowder@swri.org>
2026-03-04 16:06:44 -0700

    updates notebook to use CAM data

--- examples/animating_data.py
+++ examples/animating_data.py
@@ -17,16 +17,18 @@ from punchbowl.data.visualize import animate_punch
 
 # %% [markdown]
 # Next we'll query a sample of data to animate. Note that you can modify the time range, the product code, and the data version code.
+# Two useful datasets to visualize are CAM and PAM - level 3 clear and polarized low-noise mosaics.
 
 # %%
-start_time, end_time = datetime(2025, 10, 31, 2, 0, 0), datetime(2025, 10, 31, 3, 0, 0)
-product_code = "L3_CIM"
-version_code = "v0i"
+start_time, end_time = datetime(2025, 10, 31, 0, 0, 0), datetime(2025, 10, 31, 2, 0, 0)
+product_code = "L3_CAM"    # Or L3_PAM for polarized data
+data_type = "Unpolarized"  # Or "Polar_BpB" for polarized data
+version_code = "v0j"
 
 result = Fido.search(
                 attrs.Time(start_time, end_time),
                 attrs.Source("PUNCH-MOSAIC"),
-                attrs.Instrument("Unpolarized")
+                attrs.Instrument(data_type)
             )
 
 result = result[0][[i for i, r in enumerate(result[0]['fileid']) if product_code in r and version_code in r]]
@@ -43,11 +45,11 @@ files = Fido.fetch(result)
 # That file list is then passed into the animator function, along with an output filename.
 
 # %%
-animate_punch(files, output_path="PUNCH_CIM.mp4")
+animate_punch(files, output_path="PUNCH_CAM.mp4")
 
 # %% [markdown]
 # .. raw:: html
 #
 #    <video width="100%" controls>
-#      <source src="PUNCH_CIM.mp4" type="video/mp4">
+#      <source src="PUNCH_CAM.mp4" type="video/mp4">
 #    </video>

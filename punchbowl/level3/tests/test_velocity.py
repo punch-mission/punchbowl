0525137b3f52f992cd46d095a76f920466695e1d
s0larish <risht42@gmail.com>
2026-03-30 16:39:57 -0600

    Updated test_velocity.py

--- punchbowl/level3/tests/test_velocity.py
+++ punchbowl/level3/tests/test_velocity.py
@@ -135,6 +135,7 @@ def test_sample_radial_outflows(tmpdir):
         meta['DATE-BEG'] = "2024-01-01T00:00:00"
         meta['DATE-END'] = "2024-01-01T00:00:00"
         meta['DATE-AVG'] = "2024-01-01T00:00:00"
+        meta["OBS-MODE"] = "Polarized"
 
         # Create NDCube
         uncertainty = StdDevUncertainty(np.zeros_like(radial_outflow_data))

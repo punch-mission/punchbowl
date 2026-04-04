5d504fe452eb0c2f890d81274591f5e8b12ca3f7
Marcus Hughes <hughes.jmb@gmail.com>
2026-04-03 22:15:05 -0600

    pass is_polarized into subtract starfield

--- punchbowl/level3/flow.py
+++ punchbowl/level3/flow.py
@@ -105,11 +105,9 @@ def level3_core_flow(data_list: list[str] | list[NDCube],
     """Level 3 CTM flow."""
     logger = get_run_logger()
 
-    is_polarized = data_list[0].meta["TYPECODE"].value == "PT"
-    kind = "PTM" if is_polarized else "CTM"
-
-    logger.info(f"beginning level 3 {kind} flow")
+    logger.info("beginning level 3 flow")
     data_list = [load_image_task(d) if isinstance(d, str) else d for d in data_list]
+    is_polarized = data_list[0].meta["TYPECODE"].value == "PT"
     data_list = [subtract_starfield_background_task(d,
                                                     starfield_background_path,
                                                     is_polarized=is_polarized) for d in data_list]
@@ -139,7 +137,7 @@ def level3_core_flow(data_list: list[str] | list[NDCube],
     if output_filename is not None:
         output_image_task(out_data_list[0], output_filename)
 
-    logger.info(f"ending level 3 {kind} core flow")
+    logger.info("ending level 3 core flow")
 
     return out_data_list
 

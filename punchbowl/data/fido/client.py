bda94075f9c95961820a2ae4003fc23ed5536abe
Sam Van Kooten <vankooten.sam@gmail.com>
2026-04-27 11:29:08 -0600

    Fix Fido client version filtering

--- punchbowl/data/fido/client.py
+++ punchbowl/data/fido/client.py
@@ -154,6 +154,8 @@ class PUNCHClient(GenericClient):
                 filesmeta = []
                 for key in sorted(newest_file_by_date.keys()):
                     filesmeta.append(newest_file_by_date[key][1])
+            else:
+                filesmeta = [f for f in filesmeta if f['DataVersion'] == dversion]
 
             for row in filesmeta:
                 rowdict = self.post_search_hook(row, matchdict)

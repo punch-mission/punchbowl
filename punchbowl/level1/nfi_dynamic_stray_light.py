79a9e3403d2284cbf93a3b2e8d507f6a7ac64448
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2026-06-26 16:54:45 -0600

    (#994; callable function): add step for creating stray light model

--- punchbowl/level1/nfi_dynamic_stray_light.py
+++ punchbowl/level1/nfi_dynamic_stray_light.py
@@ -60,20 +60,52 @@ def glint_mask(data_shape,
 
 	return mask
 
+def get_solver_inputs(data_cube,
+                      smask:np.array,
+                      bindown_shape:list=[512,512]):
+	data_uncertainty = data_cube.uncertainty.array
+	data_only = data_cube.data
+	data_err = data_only*data_uncertainty
+	msk = np.isfinite(data_only)*np.isfinite(data_err)
+
+	mask = smask*msk
+	nmask = np.clip(bindown(mask,bindown_shape),1,None)
+
+	dsol = np.array([(bindown(mask*data_only,bindown_shape)/nmask).T])
+	esol = np.array([((bindown(mask*data_err**2,bindown_shape))**0.5/nmask).T])
+	gsol = np.array([(bindown(mask,bindown_shape) > 0).T])
+
+	esol += 0.01*np.abs(dsol)+np.nanmin(dsol[gsol])*0.25 # supplement the errors with 1% of the data values
+	esol[gsol==0] = np.max(dsol[gsol])
+	esol[np.isfinite(esol)==0] = np.max(dsol[gsol]) # Some nans are still getting into the errors somehow. Grrr.
+
+	return dsol, esol, gsol
+
 def remove_nfi_stray_light(data: PUNCHCube,
                            bin_factor: int = 4,
                            fwd_mat_smooth_rad = 0.0,
                            sc1:tuple = (540,790),
                            sc2:tuple = (540,1210),
                            srad:int = 375,
-                           bottom_cut = 250):
+                           bottom_cut = 250,
+                           bindown_shape = [512,512],
+                           solver_tol=1.0e-5,
+                           sky_reg=0.1,
+                           inst_reg=0.1,
+                           stray_reg=1.0e-10):
+    
     xcens, ycens, crots = get_fwd_mat_inputs(data=data,bin_factor=bin_factor)
     data_size = [1, data.meta['NAXIS1'].value, data.meta['NAXIS2'].value]
     amats = generate_nfi_fwdmats(data_size,xcens,ycens,crots,bin_fac=bin_factor,smooth_rad=fwd_mat_smooth_rad)
-    
 
+    #TODO: Make mask optional (and/or mask generating lives outside this function)
     smask = glint_mask(data.data.shape,sc1,sc2,srad,bottom_cut)
 
+    dsol, esol, gsol = get_solver_inputs(data, smask, bindown_shape=bindown_shape)
+    soln_sky, soln_ins, soln_stray, soln_dat = reconstruct_nfi_straylight(dsol, esol, amats, gsol,
+                                                                          solver_tol=solver_tol, 
+                                                                          sky_reg=sky_reg, 
+                                                                          inst_reg=inst_reg,
+                                                                          stray_reg=stray_reg)
     
-
     return

5207c0485d69c182121906c582ac24b874669820
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2026-07-02 15:34:05 -0600

    cont. cleaning the code I wrote
    adding documentation and cleaning up a few functions/variables

--- punchbowl/level1/nfi_dynamic_stray_light.py
+++ punchbowl/level1/nfi_dynamic_stray_light.py
@@ -16,17 +16,17 @@ from nfi_modules.util import bindown
 from punchbowl.data.punchcube import PUNCHCube
 
 
-def get_center(crval,cdelt,bin_factor:int):
+def get_bin_down_crval(crval,cdelt,bin_factor:int):
     """
     Calculates new sky coordinate value of reference pixel based on bin factor.
 
     Parameters
     ----------
-    crval: float
+    crval : float
         "crval" or reference pixel (in sky coordinates) of interest 
-    cdelt: float
+    cdelt : float
         "cdelt"  (or the size of a pixel in sky coordinates) associated with "crval"
-    bin_factor: int
+    bin_factor : int
         Binning down factor
 
     Returns
@@ -50,61 +50,97 @@ def get_fwd_mat_inputs(datacube: PUNCHCube,
     Parameters
     ----------
     datacube : PUNCHcube
-        PUNCH ndcube of interest
+        PUNCH data of interest
     bin_factor : int
         Binning down factor
 
     Returns
     -------
-    xcens : float
+    x_offsets_binned : float
         The new binned down x-coordinate of the reference pixel ("crval1")
-    ycens : float
+    y_offsets_binned : float
         The new binned down y-coordinate of the reference pixel ("crval2")
-    crots_radians : float
+    crota_radians : float
         The rotation angle ("CROTA") in units of radians 
     """
     data_crval1, data_crval2 = datacube.wcs.wcs.crval
     data_cdelt1, data_cdelt2 = datacube.wcs.wcs.cdelt
 
-    xcens = get_center(data_crval1,data_cdelt1,bin_factor)
-    ycens = get_center(data_crval2,data_cdelt2,bin_factor)
+    x_offsets_binned = get_bin_down_crval(data_crval1,data_cdelt1,bin_factor)
+    y_offsets_binned = get_bin_down_crval(data_crval2,data_cdelt2,bin_factor)
 
-    crots_radians = datacube.meta['CROTA'].value*np.pi/180  # save the rotation angle in radians
+    crota_radians = datacube.meta['CROTA'].value*np.pi/180  # save the rotation angle in radians
 
-    return xcens, ycens, crots_radians
+    return x_offsets_binned, y_offsets_binned, crota_radians
 
 def glint_mask(data_shape,
-               sc1:tuple,
-               sc2:tuple,
-               srad:int,
-               bottom_cut:int):
-    xa, ya = np.indices(data_shape)
-    mask =  ((((xa-sc1[0])**2+(ya-sc1[1])**2)**0.5 > srad)*
-        ((((xa-sc2[0])**2+(ya-sc2[1])**2)**0.5 > srad))*
-        (xa>bottom_cut))
+               sphere1_center:tuple = (540, 790),
+               sphere2_center:tuple = (540, 1210),
+               sphere_radius:int = 375,
+               bottom_cut_off:int = 250):
+    """
+    Create (boolean) mask for circular glints on bottom half of NFI data. 
+    (For now the plan is just to mask out the sphere pattern until we have a better way to prefilter it.)
+
+    This creates two circular masks, given estimated sphere centers and radius (assumed to have the same radius). 
+    Also masks out a portion of the bottom of the image where the spheres don't reach the bottom of the image.
+
+    Parameters
+    ----------
+    data_shape : tuple
+        Shape of the image data 
+    sphere1_center : tuple
+        Coordinate for the center of sphere 1 
+    sphere2_center : tuple
+        Coordinate for the center of sphere 2
+    sphere_radius : int
+        Radi of sphere (same radi for both spheres)
+    bottom_cut_off : int
+        the height to which include everything above and mask out everything below
+
+    Returns
+    -------
+    mask : np.array
+        Boolean mask for masking out glint spheres
+    """
+    x_axis, y_axis = np.indices(data_shape)
+    mask =  ((((x_axis-sphere1_center[0])**2+(y_axis-sphere1_center[1])**2)**0.5 > sphere_radius) 
+             *((((x_axis-sphere2_center[0])**2+(y_axis-sphere2_center[1])**2)**0.5 > sphere_radius))
+             *(x_axis>bottom_cut_off))
 
     return mask
 
-def get_solver_inputs(datacube,
-                      smask: np.ndarray,
-                      bindown_shape=(512, 52)):
+def get_solver_inputs(datacube: PUNCHCube,
+                      glint_mask: np.array,
+                      bindown_shape: tuple = (512, 512)):
+    """
+    Calculate the inputs needed to create the light solutions for NFI, and appropriately bin down to desired
+    size.
+
+    Parameters
+    ----------
+    datacube : PUNCHcube
+        PUNCH data of interest
+    glint_mask : np.array
+        Boolean mask for masking out glint spheres
+    """
     data_uncertainty = datacube.uncertainty.array
     data_only = datacube.data
     data_err = data_only*data_uncertainty
-    msk = np.isfinite(data_only)*np.isfinite(data_err)
+    err_mask = np.isfinite(data_only)*np.isfinite(data_err) #JK note: I still don't really know what this is
 
-    mask = smask*msk
-    nmask = np.clip(bindown(mask,bindown_shape),1,None)
+    mask = glint_mask*err_mask # JK note: (apparently) a mask to flag which data is good to use inversion or not
+    binned_mask = np.clip(bindown(mask,bindown_shape),1,None)
 
-    dsol = np.array([(bindown(mask*data_only,bindown_shape)/nmask).T])
-    esol = np.array([((bindown(mask*data_err**2,bindown_shape))**0.5/nmask).T])
-    gsol = np.array([(bindown(mask,bindown_shape) > 0).T])
+    solver_data = np.array([(bindown(mask*data_only,bindown_shape)/binned_mask).T])
+    solver_err = np.array([((bindown(mask*data_err**2,bindown_shape))**0.5/binned_mask).T])
+    good_data_flags = np.array([(bindown(mask,bindown_shape) > 0).T])
 
-    esol += 0.01*np.abs(dsol)+np.nanmin(dsol[gsol])*0.25 # supplement the errors with 1% of the data values
-    esol[gsol==0] = np.max(dsol[gsol])
-    esol[np.isfinite(esol)==0] = np.max(dsol[gsol]) # Some nans are still getting into the errors somehow. Grrr.
+    solver_err += 0.01*np.abs(solver_data)+np.nanmin(solver_data[good_data_flags])*0.25 # supplement the errors with 1% of the data values
+    solver_err[good_data_flags==0] = np.max(solver_data[good_data_flags])
+    solver_err[np.isfinite(solver_err)==0] = np.max(solver_data[good_data_flags]) # Some nans are still getting into the errors somehow. Grrr.
 
-    return dsol, esol, gsol
+    return solver_data, solver_err, good_data_flags
 
 def remove_nfi_stray_light(datacube: PUNCHCube,
                            bin_factor: int = 4,
@@ -121,17 +157,17 @@ def remove_nfi_stray_light(datacube: PUNCHCube,
     
     # Generate forward matrices (kernels)
     xcens, ycens, crots = get_fwd_mat_inputs(datacube=datacube,bin_factor=bin_factor)
-    data_size = [1, datacube.meta['NAXIS1'].value, datacube.meta['NAXIS2'].value]
+    id_data_size = (1, datacube.meta['NAXIS1'].value, datacube.meta['NAXIS2'].value)
 
     # Note: The inputs for "generate_nfi_fwdmats" are expected to be np.arrays to accomodate for 
     # processing multiple files at a time and generating foward matrices in one function
     # TODO: Modify to optimize processing one file at a time w/o use of np.arrays
     # TODO: OR fix above to append to arrays in the case of processing multiple frames at a time
-    amats = generate_nfi_fwdmats(data_size,
+    amats = generate_nfi_fwdmats(id_data_size,
                                  np.array([xcens]),
                                  np.array([ycens]),
                                  np.array([crots]),
-                                 bin_fac=bin_factor,smooth_rad=fwd_mat_smooth_rad)
+                                 bin_factor=bin_factor,smooth_rad=fwd_mat_smooth_rad)
 
     # Mask out glint spheres
     #TODO: Make mask optional

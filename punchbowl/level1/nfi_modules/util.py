adc49fb0bd23248c936e7813af36d99395b8885f
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2026-07-17 16:56:55 -0600

    (#1003) remove functions that aren't being used at all in the entire nfi dynamic straylight removal
    originally Joe made this code to be extendable to other use cases, but these functions don't serve a purpose in our uses at the moment.

--- punchbowl/level1/nfi_modules/util.py
+++ punchbowl/level1/nfi_modules/util.py
@@ -4,37 +4,6 @@ import numba
 import numpy as np
 
 
-def masked_median_filter(data, mask, radius, footprint=None, missing=0, footprint_ind_offset=0):
-
-    if footprint is None:
-        if np.isscalar(radius):
-            radarr = np.array([radius] * data.ndim)
-        else:
-            radarr = radius
-        coordas = np.indices(2 * radarr + 1)
-        for i in range(len(radarr)):
-            coordas[i] = (coordas[i] - radarr[i]) / radarr[i]
-        radii = np.sum(coordas**2, axis=0) ** 0.5
-        footprint = radii <= 1
-
-    flatinds = np.ravel_multi_index(np.indices(data.shape), data.shape).flatten()
-    footprint_inds = np.ravel_multi_index(np.indices(footprint.shape), footprint.shape)
-    footprint_inds = np.array(np.unravel_index(footprint_inds[footprint], footprint.shape))
-    footprint_inds = footprint_inds.transpose(np.roll(np.arange(footprint_inds.ndim), -1))
-    data_filt = np.zeros(data.size) + missing
-    footprint_pad = np.floor(0.5 * np.array(footprint.shape)).astype(np.int32)
-    footprint_pad = np.array([footprint_pad, footprint_pad]).T
-
-    data_fppad = np.pad(data, footprint_pad)
-    dat_pad_shape = data_fppad.shape
-    data_fppad = data_fppad.flatten()
-    mask_fppad = np.pad(mask, footprint_pad).flatten()
-    tparg = np.roll(np.arange(footprint_inds.ndim), 1)
-    data_filt = _masked_medfilt_inner(
-        flatinds, data, footprint_inds, footprint_ind_offset, tparg, dat_pad_shape, data_fppad, mask_fppad, data_filt
-    )
-    return data_filt.reshape(data.shape)
-
 
 @numba.jit(fastmath=True, parallel=True, forceobj=True)
 def _masked_medfilt_inner(
@@ -140,13 +109,6 @@ def bindown(d, n):
     inds = np.ravel_multi_index(np.floor(np.indices(d.shape).T * n / np.array(d.shape)).T.astype(np.uint32), n)
     return np.bincount(inds.flatten(), weights=d.flatten(), minlength=np.prod(n)).reshape(n)
 
-
-def bindown2(d, f):
-    n = np.round(np.array(d.shape) / f).astype(np.int32)
-    inds = np.ravel_multi_index(np.floor(np.indices(d.shape).T * n / np.array(d.shape)).T.astype(np.uint32), n)
-    return np.bincount(inds.flatten(), weights=d.flatten(), minlength=np.prod(n)).reshape(n)
-
-
 def binup(d, f):
     n = np.round(np.array(d.shape) * np.round(f)).astype(np.int32)
     inds = np.ravel_multi_index(np.floor(np.indices(n).T / np.array(f)).T.astype(np.uint32), d.shape)
@@ -162,20 +124,3 @@ def record_array_to_dictionary(rec):
     as_dict(my_data[234])
     """
     return {name: rec[name] for name in rec.dtype.names}
-
-
-def get_mask_errors(dat_cube, iris_err_fac, error_cube=None, filt_thold: float = 2.5):
-    dat_arr = copy.deepcopy(dat_cube)
-    dat_filt = masked_median_filter(dat_arr, (np.isnan(dat_arr) == 0), np.array([1, 2, 1]))
-    if error_cube is None:
-        error_cube = ((iris_err_fac**2 + np.clip(dat_arr, 0, None) * iris_err_fac) ** 0.5).astype("float64")
-
-    dat_median = np.nanmedian(np.abs(dat_filt))
-    dat_mask = (
-        np.isnan(dat_arr)
-        + (dat_arr < 0 * error_cube)
-        + np.isnan(error_cube)
-        + (np.abs(dat_arr - dat_filt) > filt_thold * (dat_median + np.abs(dat_filt)))
-    ) > 0
-
-    return dat_mask, error_cube

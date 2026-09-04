24e87d8af698b57f6ba17311443d43b725e53688
Chris Lowder <chris.lowder@swri.org>
2026-09-03 21:39:06 -0600

    removes accidental new line

--- punchbowl/level3/velocity.py
+++ punchbowl/level3/velocity.py
@@ -449,7 +449,6 @@ def plot_flow_map(filename: str | None, data: PUNCHCube, cmap: str = "magma") ->
         plt.savefig(filename)
     return fig
 
-
 @punch_flow(log_prints=True, timeout_seconds=21_600)
 def track_velocity(files: list[str],
                    delta_t: int = 12,

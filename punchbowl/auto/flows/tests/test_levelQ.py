8ad165879fb142c1d29f16d5d5cc055168f1a098
Sam Van Kooten <vankooten.sam@gmail.com>
2026-09-14 17:15:55 -0600

    Scheduling fixes and test

--- punchbowl/auto/flows/tests/test_levelQ.py
+++ punchbowl/auto/flows/tests/test_levelQ.py
@@ -1,12 +1,12 @@
 import os
-from datetime import UTC, datetime
+from datetime import UTC, datetime, timedelta
 
 from freezegun import freeze_time
 from prefect.logging import disable_run_logger
 from pytest_mock_resources import create_mysql_fixture
 
 from punchbowl import __version__
-from punchbowl.auto.control.db import Base, File
+from punchbowl.auto.control.db import Base, File, FileRelationship
 from punchbowl.auto.control.util import load_pipeline_configuration
 from punchbowl.auto.flows.levelq import (
     levelq_CQM_construct_file_info,
@@ -15,6 +15,7 @@ from punchbowl.auto.flows.levelq import (
     levelq_CTM_construct_file_info,
     levelq_CTM_construct_flow_info,
     levelq_CTM_query_ready_files,
+    levelq_QNN_query_ready_files,
 )
 
 TEST_DIR = os.path.dirname(__file__)
@@ -69,15 +70,152 @@ def session_fn(session):
                        software_version="none",
                        date_obs=datetime(2023, 1, 2, 0, 0, 0))
 
+    nfi_f_corona_before_file = File(level="3",
+                                    file_type="CF",
+                                    observatory="N",
+                                    state="created",
+                                    file_version="none",
+                                    software_version="none",
+                                    date_obs=datetime(2023, 1, 2, 0, 0, 0))
+
+    nfi_f_corona_after_file = File(level="3",
+                                   file_type="CF",
+                                   observatory="N",
+                                   state="created",
+                                   file_version="none",
+                                   software_version="none",
+                                   date_obs=datetime(2023, 1, 3, 0, 0, 0))
+
+    nfi_sl_file = File(level="1",
+                       file_type="SR",
+                       observatory="4",
+                       state="created",
+                       file_version="none",
+                       software_version="none",
+                       date_obs=datetime(2023, 1, 3, 0, 0, 0))
+
+    nfi_pca_comp_file = File(level="1",
+                             file_type="AR",
+                             observatory="4",
+                             state="created",
+                             file_version="none",
+                             software_version="none",
+                             date_obs=datetime(2023, 1, 3, 0, 0, 0))
+
     session.add(level0_file)
     session.add(level1_file_not_ready)
     session.add(level1_file)
     session.add(levelQ_file)
     session.add(f_corona_before_file)
     session.add(f_corona_after_file)
+    session.add(nfi_f_corona_before_file)
+    session.add(nfi_f_corona_after_file)
+    session.add(nfi_sl_file)
+    session.add(nfi_pca_comp_file)
 
 
 db = create_mysql_fixture(Base, session_fn, session=True)
+
+
+def test_levelq_CNN_query_ready_files(db):
+    pipeline_config = {'flows': {'levelq_QNN': {"batch_size_cap": 80,
+                                                "n_batches_to_schedule": 1,
+                                                "max_gap_minutes": 17,
+                                                "median_window": 5,
+                                                "zfilter_margin": 4,
+                                                "only_last_n_days": 10}}}
+    t0 = datetime.now() - timedelta(days=5)
+    files = []
+    for i in range(200):
+        t = t0 + timedelta(minutes=i * 8)
+        file = File(level="1",
+                    file_type="XR",
+                    observatory="4",
+                    state="created",
+                    file_version="none",
+                    software_version="none",
+                    date_obs=t,
+                    date_created=t - timedelta(days=1),
+                    file_id=i + 1000)
+        files.append(file)
+        if i == 198:
+            final_gap_file1 = file
+        elif i == 197:
+            final_gap_file2 = file
+        elif i == 50:
+            mid_gap_file1 = file
+        elif i == 51:
+            mid_gap_file1 = file
+        elif i == 20:
+            ok_gap_file = file
+        elif i == 70:
+            missing_neighbor_file = file
+        elif i == 100:
+            other_missing_neighbor_file = file
+        else:
+            if i in (71, 99):
+                file.date_created = datetime.now()
+            if i == 60:
+                already_processed_file = file
+                child_file = File(level="Q",
+                    file_type="QN",
+                    observatory="N",
+                    state="created",
+                    file_version="none",
+                    software_version="none",
+                    date_obs=t,
+                    date_created=t - timedelta(days=1),
+                    file_id=99999)
+                db.add(child_file)
+                rel = FileRelationship(child=child_file.file_id, parent=file.file_id)
+                db.add(rel)
+            db.add(file)
+    db.commit()
+
+    """
+         20        50,51        70,71                     99,100                                              197,198
+    |----------------|------------|--------------------------|----------------|-----------------------------------|---|
+    |     ^          ^            ^                          ^                ^                                   ^
+          |          |            |            100 missing, 99 new,     break b/c group too large                 |
+    missing, no gap  |            |               group break                                                 2 missing
+         2 missing, group break   |                                                          group break, no 199-200 grp
+         70 missing, 71 new, group break
+    """
+    groups = levelq_QNN_query_ready_files(db, pipeline_config)
+    assert len(groups) == 1
+    assert groups[0][0].file_id == 117 + 1000
+    assert groups[0][-1].file_id == 196 + 1000
+
+    pipeline_config['flows']['levelq_QNN']['n_batches_to_schedule'] = 10
+    groups = levelq_QNN_query_ready_files(db, pipeline_config)
+    assert len(groups) == 5
+
+    assert groups[4][0].file_id == 0 + 1000
+    assert groups[4][-1].file_id == 49 + 1000
+
+    # split, 2 missing files
+
+    assert groups[3][0].file_id == 52 + 1000
+    assert groups[3][-2].file_id == 69 + 1000
+    already_processed = [f for f in groups[3] if f.file_id == already_processed_file.file_id]
+    assert len(already_processed) == 1
+    assert already_processed[0] is groups[3][-1]
+    assert not already_processed[0]._to_filter
+
+    # split, 1 missing file and a neighbor is new
+
+    assert groups[2][0].file_id == 71 + 1000
+    assert groups[2][-1].file_id == 98 + 1000
+
+    # split, 1 missing file and a neighbor is new
+
+    assert groups[1][0].file_id == 101 + 1000
+    assert groups[1][-1].file_id == 116 + 4 + 2 + 1000 # Add zfilter margin and half the median window
+
+    # split b/c following group got too large
+
+    assert groups[0][0].file_id == 117 + 1000
+    assert groups[0][-1].file_id == 196 + 1000
 
 
 def test_levelq_CQM_query_ready_files(db):

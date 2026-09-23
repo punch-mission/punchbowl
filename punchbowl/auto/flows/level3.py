d55a57c05ae4307526b7f7405ca38d9262206c92
Sam Van Kooten <vankooten.sam@gmail.com>
2026-09-23 17:07:31 -0600

    Change to getting NFI from XR4s

--- punchbowl/auto/flows/level3.py
+++ punchbowl/auto/flows/level3.py
@@ -616,7 +616,7 @@ def level3_CTM_construct_flow_info(level2_files: list[File], level3_file: File,
             "after_starfield_path": after_starfield_path,
             "nfi_wfi_divide_radius": (pipeline_config.get('nfi_wfi_divide_radius', 0)
                                       if pipeline_config['nfi_mode'] == 'pca' else None),
-            "nfi_scale_factor": pipeline_config["flows"][flow_type].get('nfi_scale_factor', 1),
+            "nfi_scale_factor": pipeline_config.get('nfi_scale_factor', 1),
         },
     )
     return Flow(
@@ -769,23 +769,23 @@ def _level3_CAMPAM_query_ready_files(session, polarized: bool, pipeline_config: 
             cleaned_ready_groups.append(group)
 
     for group in cleaned_ready_groups:
-        for file in group:
-            file._is_nfi_extra = False
         if pipeline_config['nfi_mode'] == 'pca' and not polarized:
-            main_group_ids = set(f.file_id for f in group)
             dt = func.abs(func.timestampdiff(text("second"), File.date_obs, group[0]._reference_time))
-            extra_files = (session.query(File)
+            nfi_files = (session.query(File)
                            .filter(File.state.in_(["created", "progressed"]))
                            .filter(File.level == "3")
-                           .filter(File.file_type == target_type)
-                           .filter(File.observatory == "M")
+                           .filter(File.file_type == "XR")
+                           .filter(File.observatory == "4")
                            .filter(~File.outlier)
                            .filter(dt < pipeline_config['flows'][flow_type]['nfi_search_margin_minutes'] * 60)
                            .order_by(dt.desc()).all())
-            extra_files = [f for f in extra_files if f.file_id not in main_group_ids]
-            for f in extra_files:
-                f._is_nfi_extra = True
-            group.extend(extra_files)
+            if len(nfi_files) < pipeline_config['flows'][flow_type]['min_num_nfis']:
+                logger.info(
+                    f"Sending a group with {len(group)} CTMs and no XR4s (found only {len(nfi_files)} in the DB)")
+                continue
+            nfi_files = nfi_files[:pipeline_config['flows'][flow_type]['max_num_nfis']]
+            logger.info(f"Sending a group with {len(group)} CTMs and {len(nfi_files)} XR4s")
+            group.extend(nfi_files)
 
     logger.info(f"{len(cleaned_ready_groups)} groups heading out")
     return cleaned_ready_groups
@@ -801,10 +801,13 @@ def level3_CAMPAM_construct_flow_info(level3_files: list[File], level3_file_out:
 
     call_data = json.dumps(
         {
-            "data_list": [level3_file.filename() for level3_file in level3_files if not level3_file._is_nfi_extra],
-            "nfi_extras_list": [level3_file.filename() for level3_file in level3_files if level3_file._is_nfi_extra],
+            "data_list": [level3_file.filename() for level3_file in level3_files if level3_file.file_type != 'XR'],
+            "nfi_list": [level3_file.filename() for level3_file in level3_files
+                                if level3_file.file_type == 'XR'],
             "reference_time": reference_time.strftime("%Y-%m-%dT%H:%M:%S"),
-            "nfi_wfi_divide_radius": pipeline_config.get('nfi_wfi_divide_radius', None),
+            "nfi_wfi_divide_radius": (pipeline_config.get('nfi_wfi_divide_radius', None)
+                                      if flow_type == "level3_CAM" else None),
+            "nfi_scale_factor": pipeline_config.get('nfi_scale_factor', 1),
         },
     )
     return Flow(
@@ -872,7 +875,7 @@ def level3_PAM_process_flow(flow_id: int | list[int], pipeline_config_path=None,
 
 
 def level3_CAMPAM_call_data_processor(call_data: dict, pipeline_config, session=None) -> dict:
-    for key in ["data_list", "nfi_extras_list"]:
+    for key in ["data_list", "nfi_list"]:
         if key in call_data:
             call_data[key] = file_name_to_full_path(call_data[key], pipeline_config["root"])
     return call_data
--- punchbowl/level3/flow.py
+++ punchbowl/level3/flow.py
@@ -288,19 +288,21 @@ def level3_core_flow(data_list: list[str | PUNCHCube],
 
 @punch_flow
 def generate_level3_low_noise_flow(data_list: list[str | PUNCHCube],
-                                   nfi_extras_list: list[str | PUNCHCube] | None,
+                                   nfi_list: list[str | PUNCHCube] | None,
                                    output_filename: str | None = None,
                                    nfi_wfi_divide_radius: float | None = None,
+                                   nfi_scale_factor: float = 1,
                                    reference_time: str | datetime | None = None) -> list[PUNCHCube]:
     """Generate low noise products."""
     logger = get_logger()
 
     logger.info("Generating low noise products")
     data_list = [load_image_task(d) if isinstance(d, str) else d for d in data_list]
-    if nfi_extras_list:
-        nfi_extras_list = [load_image_task(d) if isinstance(d, str) else d for d in nfi_extras_list]
-    low_noise_image = create_low_noise_task(data_list, reference_time=reference_time, extra_nfi_cubes=nfi_extras_list,
-                                            nfi_wfi_divide_radius=nfi_wfi_divide_radius)
+    if nfi_list and nfi_wfi_divide_radius:
+        nfi_list = [load_image_task(d) if isinstance(d, str) else d for d in nfi_list]
+    low_noise_image = create_low_noise_task(data_list, reference_time=reference_time, nfi_cubes=nfi_list,
+                                            nfi_wfi_divide_radius=nfi_wfi_divide_radius,
+                                            nfi_scale_factor=nfi_scale_factor)
 
     if output_filename is not None:
         output_image_task(low_noise_image, output_filename)
